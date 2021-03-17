007b2d15b65fd3e3e11a874fff6c716d72de884f
Mauro Melo <mauromelo1@gmail.com>
2021-03-17 10:36:20 -0300

    Criando repository e service para Categoria - update2

--- src/main/java/com/mauromelo1/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mauromelo1/cursomc/resources/CategoriaResource.java
@@ -19,7 +19,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
