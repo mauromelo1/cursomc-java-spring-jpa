4159514f249e4035d492f326577587aee028ab07
Mauro Melo <mauromelo1@gmail.com>
2021-03-18 13:25:54 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/mauromelo1/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mauromelo1/cursomc/resources/CategoriaResource.java
@@ -2,7 +2,6 @@ package com.mauromelo1.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
